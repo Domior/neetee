adfa36bd55336fc6bd10b3fa21e4050d87f25401
Natalia Bondarenko <lisa15.08patrikeevna@gmail.com>
2022-02-11 01:30:19 +0200

    add 8.1...

--- src/containers/pages/8.1/ConfirmationOfAnOrder.js
+++ src/containers/pages/8.1/ConfirmationOfAnOrder.js
@@ -2,31 +2,48 @@ import React, { useState } from 'react';
 import { Button, Step, StepButton, Stepper } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import cl from './ConfirmationOfAnOrder.module.css'
+import cl from './ConfirmationOfAnOrder.module.css';
 import FirstStep from './innerStaps/firstStep/firstStep';
+import StepLabel from '@mui/material/StepLabel';
+import StepContent from '@mui/material/StepContent';
 
 
 const steps = ['Данные получателя', 'Оплата товара', 'Выбор доставки','Отслеживание заказа'];
-
+const innerSteps = [
+  <FirstStep/>,
+  'some text',
+  'last',
+  'finish'
+]
 const ConfirmationOfAnOrder = () => {
 
   const [activeStep, setActiveStep] = useState(0);
   const [completed, setCompleted] = useState({});
 
-  const totalSteps = () => {return steps.length};
-  const completedSteps = () => {return Object.keys(completed).length};
-  const isLastStep = () => {return activeStep === totalSteps() - 1};
-  const handleStep = (step) => () => {setActiveStep(step)};
-  const allStepsCompleted = () => {return completedSteps() === totalSteps()};
+  const totalSteps = () => {
+    return steps.length;
+  };
+  const completedSteps = () => {
+    return Object.keys(completed).length;
+  };
+  const isLastStep = () => {
+    return activeStep === totalSteps() - 1;
+  };
+  const handleStep = (step) => () => {
+    setActiveStep(step);
+  };
+  const allStepsCompleted = () => {
+    return completedSteps() === totalSteps();
+  };
   const handleBack = () => {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
   const handleNext = () => {
     const newActiveStep = isLastStep() && !allStepsCompleted()
-        ? // It's the last step, but not all steps have been completed,
-          // find the first step that has been completed
-        steps.findIndex((step, i) => !(i in completed))
-        : activeStep + 1;
+      ? // It's the last step, but not all steps have been completed,
+        // find the first step that has been completed
+      steps.findIndex((step, i) => !(i in completed))
+      : activeStep + 1;
     setActiveStep(newActiveStep);
   };
 
@@ -41,6 +58,7 @@ const ConfirmationOfAnOrder = () => {
     setCompleted({});
   };
 
+
   return (
     <div className={cl.stepperStyle}>
       <Stepper nonLinear activeStep={activeStep}>
@@ -52,30 +70,26 @@ const ConfirmationOfAnOrder = () => {
           </Step>
         ))}
       </Stepper>
-      <div className={cl.red}>
-        {allStepsCompleted() ? (
-          <>
-            <Typography sx={{ mt: 2, mb: 1 }}>
-              All steps completed - you&apos;re finished
-            </Typography>
-            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-              <Box sx={{ flex: '1 1 auto' }} />
-              <Button onClick={handleReset}>Reset</Button>
-            </Box>
-          </>
-        ) : (
-          <>
-            {/*<div sx={{ mt: 2, mb: 1 }}>task {activeStep + 1}</div>*/}
-            <Box sx={{ mt: 8, mb: 1 }}>
-              <FirstStep/>
-            </Box>
-
-          </>
-        )}
-      </div>
-
-
-      ConfirmationOfAnOrder
+        <Box className={cl.red}>
+          {allStepsCompleted() ? (
+            <>
+              <Typography sx={{ mt: 2, mb: 1 }}>
+                All steps completed - you&apos;re finished
+              </Typography>
+              <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                <Box sx={{ flex: '1 1 auto' }} />
+                <Button onClick={handleReset}>Reset</Button>
+              </Box>
+            </>
+          ) : (
+            <>
+              {/*<div sx={{ mt: 2, mb: 1 }}>task {activeStep + 1}</div>*/}
+                <Box sx={{ mt: 8, mb: 1 }}>
+                  {innerSteps[activeStep]}
+                </Box>
+            </>
+          )}
+        </Box>
     </div>
   );
 };
