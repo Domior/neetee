5fa70543f865871090eb7e668ab4ecedfd60d079
Andrew Snisar <andrew.snisar@gmail.com>
2022-03-09 20:29:29 +0200

    created cersion for submenu

--- src/components/HeaderSubmenu/HeaderSubmenu.jsx
+++ src/components/HeaderSubmenu/HeaderSubmenu.jsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 // import { Link } from 'react-router-dom';
 
-import { makeStyles } from '@mui/styles';
+import { makeStyles, useTheme } from '@mui/styles';
 
 import { CSSTransition } from 'react-transition-group';
+
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 import { ReactComponent as MenuButtonIcon } from '@icons/menu.svg';
 import { ReactComponent as ArrowIcon } from '@icons/arrow.svg';
@@ -12,35 +14,39 @@ import './HeaderSubmenu.module.css';
 
 const useStyles = makeStyles((theme) => ({
   navbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
     position: 'relative',
     height: 'inherit',
     padding: `0 20px`,
+    [theme.breakpoints.down('md')]: {
+      padding: `0 20px`,
+    },
   },
-  navbarNav: {
-    maxWidth: '100%',
-    height: 'inherit',
-    display: 'flex',
-    justifyContent: 'flex-start',
+  dropdown: {
+    width: '100%',
+    [theme.breakpoints.down('md')]: {
+      position: 'absolute',
+      top: '29px',
+      left: 0,
+      zIndex: 100,
+      width: '100%',
+      maxHeight: '180px',
+      overflowY: 'auto',
+      backgroundColor: '#C4C4C4',
+      transition: `height .5s ease`,
+      padding: `0 0 20px`,
+    },
   },
-  navItem: {
+  menu: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  dropdown: {
-    position: 'absolute',
-    top: '29px',
-    left: 0,
-    zIndex: 100,
     width: '100%',
-    maxHeight: '180px',
-    overflowY: 'auto',
-    backgroundColor: '#C4C4C4',
-    transition: `height .5s ease`,
-    padding: `0 0 20px`,
-  },
-  menu: {
-    width: '100%',
+    [theme.breakpoints.down('md')]: {
+      display: 'block',
+    },
   },
 
   menuItem: {
@@ -85,12 +91,14 @@ const useStyles = makeStyles((theme) => ({
 
 export default function HeaderSubmenu() {
   const [open, setOpen] = React.useState(false);
+  const theme = useTheme();
+  const isMobileScreen = useMediaQuery(theme.breakpoints.down('md'));
   const classes = useStyles();
 
   return (
     <nav className={classes.navbar}>
-      <ul className={classes.navbarNav}>
-        <li className={classes.navItem}>
+      {isMobileScreen && (
+        <>
           <a
             className={classes.menuIcon}
             href="#/"
@@ -103,8 +111,9 @@ export default function HeaderSubmenu() {
             )}
           </a>
           {open && <DropdownMenu />}
-        </li>
-      </ul>
+        </>
+      )}
+      {!isMobileScreen && <DropdownMenu />}
     </nav>
   );
 }
@@ -112,14 +121,32 @@ export default function HeaderSubmenu() {
 function DropdownMenu() {
   const [activeMenu, setActiveMenu] = React.useState('main');
   const dropdownRef = React.useRef(null);
+  const dropdownItemRef = React.useRef(null);
+  const theme = useTheme();
+  const isMobileScreen = useMediaQuery(theme.breakpoints.down('md'));
   const classes = useStyles();
 
   function DropdownItem(props) {
+    const handleDropdownItemClick = () => {
+      if (isMobileScreen) {
+        props.goToMenu && setActiveMenu(props.goToMenu);
+      }
+    };
+    const handleDropdownItemHover = (event) => {
+      if (!isMobileScreen) {
+        if (!event.target.hasAttribute('href')) {
+          props.goToMenu && setActiveMenu(props.goToMenu);
+          console.log(dropdownItemRef);
+        }
+      }
+    };
     return (
       <a
         href={props.link}
         className={classes.menuItem}
-        onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}
+        onClick={handleDropdownItemClick}
+        onMouseEnter={handleDropdownItemHover}
+        onMouseLeave={() => console.log('unhovered')}
       >
         {props.children}
       </a>
@@ -147,14 +174,17 @@ function DropdownMenu() {
         timeout={500}
         classNames="menu-secondary"
         unmountOnExit
+        ref={dropdownItemRef}
       >
         <div className={classes.menu}>
-          <DropdownItem goToMenu="main">
-            <div className={classes.goBackbutton}>
-              <ArrowIcon />
-              <p>Назад</p>
-            </div>
-          </DropdownItem>
+          {isMobileScreen && (
+            <DropdownItem goToMenu="main">
+              <div className={classes.goBackbutton}>
+                <ArrowIcon />
+                <p>Назад</p>
+              </div>
+            </DropdownItem>
+          )}
           <DropdownItem link={'#'}>Как продавать</DropdownItem>
           <DropdownItem link={'#'}>Как заказать</DropdownItem>
           <DropdownItem link={'#'}>Оплата</DropdownItem>
