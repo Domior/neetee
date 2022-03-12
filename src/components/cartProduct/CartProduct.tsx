12ba79952458e9bbf9274d25a95992de51981d70
Natalia Bondarenko <lisa15.08patrikeevna@gmail.com>
2022-03-12 11:02:03 +0200

    cartProduct

---
+++ src/components/cartProduct/CartProduct.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import cl from './'
+
+const CartProduct = () => {
+  return (
+    <div className={cl.cartProductWrap}>
+      
+    </div>
+  );
+};
+
+export default CartProduct;
