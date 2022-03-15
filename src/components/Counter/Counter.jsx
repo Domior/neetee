5998b48bf5a5966fc7e4fb25065f887f07af3f67
Andrew Snisar <andrew.snisar@gmail.com>
2022-03-15 20:21:50 +0200

    fixed counter

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -11,7 +11,7 @@ const useStyles = makeStyles((theme) => ({
   counterContainer: {
     display: 'flex',
     height: '50px',
-    border: `1px solid ${theme.palette.primary.secondary}`,
+    border: `2px solid ${theme.palette.primary.secondary}`,
     borderRadius: '5px',
     backgroundColor: theme.colors.white,
     [theme.breakpoints.down('md')]: {
@@ -25,8 +25,17 @@ const useStyles = makeStyles((theme) => ({
     color: `${theme.colors.black}`,
     padding: '0 20px',
     minWidth: 'unset',
+    borderRadius: 0,
     '& svg': {
       fontSize: 'unset',
+    },
+    '&:active, &:hover': {
+      backgroundColor: '#4270AD',
+      transition: `all .5s linear`,
+    },
+    '&:active svg, &:hover svg': {
+      fill: '#ffff',
+      transition: `all .5s ease-out`,
     },
     [theme.breakpoints.down('md')]: {
       fontSize: '14px',
@@ -34,50 +43,77 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   counter: {
+    width: '70px',
     fontSize: '14px',
     fontWeight: '500',
     lineHeight: '16px',
     color: `${theme.colors.black} !important`,
-    borderLeft: `1px solid ${theme.palette.primary.secondary}`,
-    borderRight: `1px solid ${theme.palette.primary.secondary}`,
+    border: 'none',
+    borderLeft: `2px solid ${theme.palette.primary.secondary}`,
+    borderRight: `2px solid ${theme.palette.primary.secondary}`,
     borderRadius: 'unset',
-    padding: '0 30px',
+    textAlign: 'center',
     minWidth: 'unset',
+    backgroundColor: 'transparent',
+    outline: 'none',
     [theme.breakpoints.down('md')]: {
       fontSize: '10px',
       lineHeight: '12px',
-      padding: '0 18px',
+      width: '50px',
+    },
+  },
+  active: {
+    borderColor: '#4270AD',
+    transition: `all .5s linear`,
+    '& input': {
+      borderColor: '#4270AD',
+      transition: `all .5s linear`,
     },
   },
 }));
 
 const Counter = () => {
   const [count, setCount] = React.useState(1);
+  const [active, setActive] = React.useState(null);
 
   const classes = useStyles();
 
   return (
     <div
-      className={classes.counterContainer}
+      className={`
+        ${classes.counterContainer}
+        ${active === 'active' ? classes.active : ''}
+      `}
       aria-label="counter-buttons-group"
+      onMouseLeave={() => setActive('')}
     >
       <Button
         className={classes.counterBtn}
         disabled={count <= 1}
-        onClick={() => {
+        onMouseDown={() => {
+          // setActive('active');
           setCount(count - 1);
         }}
+        // onMouseUp={() => setActive('')}
+        onMouseEnter={() => setActive('active')}
       >
         <RemoveIcon />
       </Button>
-      <Button className={classes.counter} disabled>
-        {count}
-      </Button>
+      <input
+        className={classes.counter}
+        value={count}
+        onChange={(e) => setCount(+e.target.value)}
+        onFocus={() => setActive('active')}
+        onBlur={() => setActive('')}
+      />
       <Button
         className={classes.counterBtn}
-        onClick={() => {
+        onMouseDown={() => {
+          setActive('active');
           setCount(count + 1);
         }}
+        // onMouseUp={() => setActive('')}
+        onMouseEnter={() => setActive('active')}
       >
         <AddIcon />
       </Button>
